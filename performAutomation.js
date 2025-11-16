12def554c1386e478a426ee95559f8cd8a00e3ae
Daraphista <81943522+Daraphista@users.noreply.github.com>
2025-11-17 04:40:48 +0800

    Update performAutomation.js

--- performAutomation.js
+++ performAutomation.js
@@ -2,6 +2,7 @@ import { loadCookiesFromEnv } from "./cookieHelper.js";
 import { preloadCookies } from "./cookiePreloader.js";
 import Steel from "steel-sdk";
 import { chromium } from "playwright";
+import { steelConfig } from "./steelConfig.js";
 
 export async function performAutomation({ url }) {
   const STEEL_API_KEY = process.env.STEEL_API_KEY;
@@ -9,7 +10,7 @@ export async function performAutomation({ url }) {
 
   // Create Steel session
   const client = new Steel({ steelAPIKey: STEEL_API_KEY });
-  const session = await client.sessions.create();
+  const session = await client.sessions.create({ config: steelConfig });
 
   const wsUrl = session.websocketUrl.includes("apiKey=")
     ? session.websocketUrl
